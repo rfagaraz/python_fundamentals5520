0e4ab4661013770614dc47c425bb2832c227c4e0
Renato Fagaraz <rfagaraz@gmail.com>
2020-03-04 22:34:44 -0300

    Aula 0

---
+++ Aulas/basic.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# nome = 'Renato'
+# idade = 26
+# altura = 1.70
+# brasileiro = True
+
+# print(nome)
+# print('Olá, meu nome é', nome)          # Primeira opção 
+# print('Olá, meu nome é {}'.format(nome))# Segunda opção
+# print(f'Olá, meu nome é {nome}')        # Terceira opção
+
+# Tipos de dados
+# print(type(nome))
+# print(type(idade))
+# print(type(altura))
+# print(type(brasileiro))
+
+# Entrada de dados
+# nome = input('Digite seu nome: ')
+# print(f'O nome do usuário é {nome}')
+
+# num1 = input('Digite o primeiro número: ')
+# num2 = input('Digite o segundo número: ')
+# print(int(num1)+float(num2))
+
+texto = 'esse é um texto'
+
+print(texto.upper())
+print(texto.lower())
+print(texto.capitalize())
+print(texto.split(' '))
---
+++ Exercícios/basic_ex.py
@@ -0,0 +1,12 @@
+# 1. Crie um script com duas variáveis:
+# nome, sobrenome
+# e imprima na tela a mensagem "Olá, meu nome é 'nome' sobrenome'"
+
+nome = 'Renato'
+sobrenome = 'Fagaraz'
+
+print(f'Olá, meu nome é {nome} {sobrenome}')
+print('Olá, meu nome é {} {}'.format(nome, sobrenome))
+print('Olá, meu nome é',nome, sobrenome)
+
+print('Olá, meu nome é ' + nome + ' ' + sobrenome)
